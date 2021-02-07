ae9138b2b3ad6e459d0a80a532b49df4d56234be
Park Sangwoo <tkddnsos@gmail.com>
2021-02-07 23:37:39 +0900

    [create]BOJ_1476_날짜계산

---
+++ src/final_algo/boj/BOJ_1476_날짜계산.java
@@ -0,0 +1,23 @@
+package final_algo.boj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_1476_날짜계산 {
+
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int E = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		int year = 0;
+		while(true) {
+			year ++;
+			if((year-E)%15==0 && (year-S)%28==0 && (year-M)%19==0)break;
+		}
+		System.out.println(year);
+	}
+}
