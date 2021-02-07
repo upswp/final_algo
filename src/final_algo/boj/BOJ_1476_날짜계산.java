8225911c61792b3ebcab09e9c759df7812d6f569
Park Sangwoo <tkddnsos@gmail.com>
2021-02-07 23:40:01 +0900

    [update]BOJ_1476_날짜계산

--- src/final_algo/boj/BOJ_1476_날짜계산.java
+++ src/final_algo/boj/BOJ_1476_날짜계산.java
@@ -3,7 +3,7 @@ package final_algo.boj;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
-
+/*Silver 5*/
 public class BOJ_1476_날짜계산 {
 
 	public static void main(String[] args)throws Exception {
