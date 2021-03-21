09b4d0ea5abdea26f07d335898a562fa1ecbe8e2
Park Sangwoo <tkddnsos@gmail.com>
2021-03-21 23:37:41 +0900

    [create]Programmers_신규아이디추천

---
+++ src/final_algo/programmers/Programmers_신규아이디추천.java
@@ -0,0 +1,12 @@
+package final_algo.programmers;
+
+public class Programmers_신규아이디추천 {
+
+	class Solution {
+	    public String solution(String new_id) {
+	        String answer = "";
+	        return answer;
+	    }
+	}
+
+}
