a306c312e66729b0985e0dfb46e022277a8b32d1
Park Sangwoo <tkddnsos@gmail.com>
2021-03-26 02:15:58 +0900

    [create]BOJ_1001_A-B

---
+++ src/final_algo/boj/BOJ_1001_A빼기B.java
@@ -0,0 +1,15 @@
+package final_algo.boj;
+
+import java.util.Scanner;
+
+public class BOJ_1001_A빼기B {
+
+	static int A, B;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		System.out.println(A-B);
+	}
+
+}
