055e5e3b4b11f2fd5d142aaab096b0f018a08891
Park Sangwoo <tkddnsos@gmail.com>
2021-03-26 02:16:10 +0900

    [create]BOJ_1008_A/B

---
+++ src/final_algo/boj/BOJ_1008_A나누기B.java
@@ -0,0 +1,14 @@
+package final_algo.boj;
+
+import java.util.Scanner;
+
+public class BOJ_1008_A나누기B {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		System.out.println((double)A/(double)B);
+	}
+
+}
