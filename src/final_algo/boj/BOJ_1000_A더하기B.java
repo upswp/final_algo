8b575ab106cca2a0f8fe58d55689fb157581eedc
Park Sangwoo <tkddnsos@gmail.com>
2021-03-26 02:15:47 +0900

    [create]BOJ_1000_A+B

---
+++ src/final_algo/boj/BOJ_1000_A더하기B.java
@@ -0,0 +1,15 @@
+package final_algo.boj;
+
+import java.util.Scanner;
+
+public class BOJ_1000_A더하기B {
+
+	static int A, B;
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		System.out.println(A+B);
+	}
+
+}
