98b98ae0726b70aa75bfd94722afa023c57e6767
Park Sangwoo <tkddnsos@gmail.com>
2021-03-28 23:32:45 +0900

    [create]BOJ_11654_아스키코드

---
+++ src/final_algo/boj/BOJ_11654_아스키코드.java
@@ -0,0 +1,13 @@
+package final_algo.boj;
+
+import java.util.Scanner;
+
+public class BOJ_11654_아스키코드 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int answer = sc.next().charAt(0);
+		System.out.println(answer);
+	}
+
+}
