425eccf7d643ac4537afcf3490b15c01edb17ef7
Park Sangwoo <tkddnsos@gmail.com>
2021-07-08 23:36:59 +0900

    [create]Programmers_모의고사

---
+++ src/final_algo/programmers/Programmers_모의고사.java
@@ -0,0 +1,10 @@
+package final_algo.programmers;
+
+public class Programmers_모의고사 {
+    class Solution {
+        public int[] solution(int[] answers) {
+            int[] answer = {};
+            return answer;
+        }
+    }
+}
